a70f18619a43256f69a3ef38072c2968d91da5de
Narasimha Karthik <narasimhakarthik2@gmail.com>
2019-04-18 19:40:09 +0530

    weather app

---
+++ app/src/main/java/com/example/karthik/sunshine/utilities/SunshineDateUtils.java
@@ -0,0 +1,207 @@
+package com.example.karthik.sunshine.utilities;
+
+import android.content.Context;
+import android.text.format.DateUtils;
+
+
+import com.example.karthik.sunshine.R;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Class for handling date conversions that are useful for Sunshine.
+ */
+public final class SunshineDateUtils {
+
+    /* Milliseconds in a day */
+    public static final long DAY_IN_MILLIS = TimeUnit.DAYS.toMillis(1);
+
+
+    public static long getNormalizedUtcDateForToday() {
+
+        /*
+         * This number represents the number of milliseconds that have elapsed since January
+         * 1st, 1970 at midnight in the GMT time zone.
+         */
+        long utcNowMillis = System.currentTimeMillis();
+
+        /*
+         * This TimeZone represents the device's current time zone. It provides us with a means
+         * of acquiring the offset for local time from a UTC time stamp.
+         */
+        TimeZone currentTimeZone = TimeZone.getDefault();
+
+        /*
+         * The getOffset method returns the number of milliseconds to add to UTC time to get the
+         * elapsed time since the epoch for our current time zone. We pass the current UTC time
+         * into this method so it can determine changes to account for daylight savings time.
+         */
+        long gmtOffsetMillis = currentTimeZone.getOffset(utcNowMillis);
+
+        /*
+         * UTC time is measured in milliseconds from January 1, 1970 at midnight from the GMT
+         * time zone. Depending on your time zone, the time since January 1, 1970 at midnight (GMT)
+         * will be greater or smaller. This variable represents the number of milliseconds since
+         * January 1, 1970 (GMT) time.
+         */
+        long timeSinceEpochLocalTimeMillis = utcNowMillis + gmtOffsetMillis;
+
+        /* This method simply converts milliseconds to days, disregarding any fractional days */
+        long daysSinceEpochLocal = TimeUnit.MILLISECONDS.toDays(timeSinceEpochLocalTimeMillis);
+
+        /*
+         * Finally, we convert back to milliseconds. This time stamp represents today's date at
+         * midnight in GMT time. We will need to account for local time zone offsets when
+         * extracting this information from the database.
+         */
+        long normalizedUtcMidnightMillis = TimeUnit.DAYS.toMillis(daysSinceEpochLocal);
+
+        return normalizedUtcMidnightMillis;
+    }
+
+    /**
+     * This method returns the number of days since the epoch (January 01, 1970, 12:00 Midnight UTC)
+     * in UTC time from the current date.
+     *
+     * @param utcDate A date in milliseconds in UTC time.
+     *
+     * @return The number of days from the epoch to the date argument.
+     */
+    private static long elapsedDaysSinceEpoch(long utcDate) {
+        return TimeUnit.MILLISECONDS.toDays(utcDate);
+    }
+
+
+    public static long normalizeDate(long date) {
+        long daysSinceEpoch = elapsedDaysSinceEpoch(date);
+        long millisFromEpochToTodayAtMidnightUtc = daysSinceEpoch * DAY_IN_MILLIS;
+        return millisFromEpochToTodayAtMidnightUtc;
+    }
+
+
+    public static boolean isDateNormalized(long millisSinceEpoch) {
+        boolean isDateNormalized = false;
+        if (millisSinceEpoch % DAY_IN_MILLIS == 0) {
+            isDateNormalized = true;
+        }
+
+        return isDateNormalized;
+    }
+
+
+    private static long getLocalMidnightFromNormalizedUtcDate(long normalizedUtcDate) {
+        /* The timeZone object will provide us the current user's time zone offset */
+        TimeZone timeZone = TimeZone.getDefault();
+        /*
+         * This offset, in milliseconds, when added to a UTC date time, will produce the local
+         * time.
+         */
+        long gmtOffset = timeZone.getOffset(normalizedUtcDate);
+        long localMidnightMillis = normalizedUtcDate - gmtOffset;
+        return localMidnightMillis;
+    }
+
+
+    public static String getFriendlyDateString(Context context, long normalizedUtcMidnight, boolean showFullDate) {
+
+        long localDate = getLocalMidnightFromNormalizedUtcDate(normalizedUtcMidnight);
+
+        /*
+         * In order to determine which day of the week we are creating a date string for, we need
+         * to compare the number of days that have passed since the epoch (January 1, 1970 at
+         * 00:00 GMT)
+         */
+        long daysFromEpochToProvidedDate = elapsedDaysSinceEpoch(localDate);
+
+        /*
+         * As a basis for comparison, we use the number of days that have passed from the epoch
+         * until today.
+         */
+        long daysFromEpochToToday = elapsedDaysSinceEpoch(System.currentTimeMillis());
+
+        if (daysFromEpochToProvidedDate == daysFromEpochToToday || showFullDate) {
+            /*
+             * If the date we're building the String for is today's date, the format
+             * is "Today, June 24"
+             */
+            String dayName = getDayName(context, localDate);
+            String readableDate = getReadableDateString(context, localDate);
+            if (daysFromEpochToProvidedDate - daysFromEpochToToday < 2) {
+                /*
+                 * Since there is no localized format that returns "Today" or "Tomorrow" in the API
+                 * levels we have to support, we take the name of the day (from SimpleDateFormat)
+                 * and use it to replace the date from DateUtils. This isn't guaranteed to work,
+                 * but our testing so far has been conclusively positive.
+                 *
+                 * For information on a simpler API to use (on API > 18), please check out the
+                 * documentation on DateFormat#getBestDateTimePattern(Locale, String)
+                 * https://developer.android.com/reference/android/text/format/DateFormat.html#getBestDateTimePattern
+                 */
+                String localizedDayName = new SimpleDateFormat("EEEE").format(localDate);
+                return readableDate.replace(localizedDayName, dayName);
+            } else {
+                return readableDate;
+            }
+        } else if (daysFromEpochToProvidedDate < daysFromEpochToToday + 7) {
+            /* If the input date is less than a week in the future, just return the day name. */
+            return getDayName(context, localDate);
+        } else {
+            int flags = DateUtils.FORMAT_SHOW_DATE
+                    | DateUtils.FORMAT_NO_YEAR
+                    | DateUtils.FORMAT_ABBREV_ALL
+                    | DateUtils.FORMAT_SHOW_WEEKDAY;
+
+            return DateUtils.formatDateTime(context, localDate, flags);
+        }
+    }
+
+    /**
+     * Returns a date string in the format specified, which shows an abbreviated date without a
+     * year.
+     *
+     * @param context      Used by DateUtils to format the date in the current locale
+     * @param timeInMillis Time in milliseconds since the epoch (local time)
+     *
+     * @return The formatted date string
+     */
+    private static String getReadableDateString(Context context, long timeInMillis) {
+        int flags = DateUtils.FORMAT_SHOW_DATE
+                | DateUtils.FORMAT_NO_YEAR
+                | DateUtils.FORMAT_SHOW_WEEKDAY;
+
+        return DateUtils.formatDateTime(context, timeInMillis, flags);
+    }
+
+    /**
+     * Given a day, returns just the name to use for that day.
+     *   E.g "today", "tomorrow", "Wednesday".
+     *
+     * @param context      Context to use for resource localization
+     * @param dateInMillis The date in milliseconds (UTC time)
+     *
+     * @return the string day of the week
+     */
+    private static String getDayName(Context context, long dateInMillis) {
+        /*
+         * If the date is today, return the localized version of "Today" instead of the actual
+         * day name.
+         */
+        long daysFromEpochToProvidedDate = elapsedDaysSinceEpoch(dateInMillis);
+        long daysFromEpochToToday = elapsedDaysSinceEpoch(System.currentTimeMillis());
+
+        int daysAfterToday = (int) (daysFromEpochToProvidedDate - daysFromEpochToToday);
+
+        switch (daysAfterToday) {
+            case 0:
+                return context.getString(R.string.today);
+            case 1:
+                return context.getString(R.string.tomorrow);
+
+            default:
+                SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+                return dayFormat.format(dateInMillis);
+        }
+    }
+}
